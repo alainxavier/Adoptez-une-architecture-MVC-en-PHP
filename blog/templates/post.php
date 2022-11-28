9871dc48ecbc5b9d1b32068eb22c4cac21cc3881
alainxavier <mekkaddish@outlook.com>
2022-11-28 17:07:35 +0000

    Afficher des commentaires

--- blog/templates/post.php
+++ blog/templates/post.php
@@ -8,25 +8,26 @@
 
    <body>
       <h1>Le super blog de l'AVBN !</h1>
-      <p>Derniers billets du blog :</p>
-
-      <?php
-      foreach($posts as $post) {
-        ?>
+      <p><a href="index.php">Retour à la liste des billets du blog</a></p>
 
       <div class="news">
          <h3>
             <?= htmlspecialchars($post['title']); ?>
             <em>le <?= $post['french_creation_date']; ?></em>
          </h3>
+
          <p>
          <?=nl2br(htmlspecialchars($post['content']));?>
-         <br />
-         <em><a href="post.php?id=<?= urlencode($post['identifier'] ?>">Commentaires</a></em>
          </p>
       </div>
+      <h2>Commentaires</h2>
       <?php
-      } 
+      foreach ($comments as $comment) {
+        ?>
+        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['french_creation_date'] ?></p>
+        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+        <?php
+      }
       ?>
    </body>
 </html>
